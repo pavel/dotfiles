6308b6c017c4d3020c555da206904058637dc198
pavel <pavel.github@outlook.com>
2017-05-15 09:48:06 -0400

    Added fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if not set -q LANG > /dev/null
 	set -gx LANG en_US.UTF-8
 end
+source ~/.config/fish/theme.fish
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
 alias startx "ssh-agent startx"
 alias e "nvim"
